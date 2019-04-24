5d9da82eb73f0334ffa7d1d653d954d99383eff6
wxin_bxbank <weixin@aibank.com>
2019-04-24 21:45:16 +0800

    rsaa

--- association_rules/rsaa.py
+++ association_rules/rsaa.py
@@ -14,12 +14,13 @@ class RelativeSupportApriori:
         self.freq_dict = defaultdict(int)
         self.F1 = set()  # F==L,
         self.R1 = set()  # R==NL
+        self.rule_input = defaultdict(int)
 
     def get_candidate_itemsets(self):
-        ret = set()
         for trans in self.data:
             for item in trans:
                 self.freq_dict[tuple([item])] += 1
+                self.rule_input[frozenset({item})] += 1
 
         self.F1 = set([item for item, support in self.freq_dict.items() if support >= self.max_sup])
         self.R1 = set([item for item, support in self.freq_dict.items() if self.max_sup > support >= self.min_sup])
@@ -36,35 +37,33 @@ class RelativeSupportApriori:
             NLC = self.rsaa_gen(NLL, k)
             NL = self.gen_quasi(NC)
             NLL = self.gen_quasi(NLC)
-            ret.union(NL)
-            ret.union(NLL)
             k += 1
 
-        return ret
+        return self.rule_input
 
     def rsaa_gen(self, itemsets, k):
         if k == 2:
             candidate = [tuple(sorted([x[0], y[0]])) for x in itemsets for y in itemsets if
                          len((x[0], y[0])) == k and x[0] != y[0]]
         else:
-            candidate = [tuple(sorted(set(x).union(y))) for x in itemsets for y in itemsets if
-                         list(x)[-1] != list(y)[-1] and list(x)[0:-1] == list(y)[0:-1]]
+            candidate = list(itertools.combinations(itemsets, 2))
+            candidate = [set(i[0]).union(i[1]) for i in candidate]
+            candidate = [tuple(sorted(i)) for i in candidate if len(i) == k]
 
         candidate = list(set(candidate))
-        for c in candidate:
-            subsets = self.gen_subsets(c, k - 1)
+        for idx in range(len(candidate)-1, -1, -1):
+            subsets = self.gen_subsets(candidate[idx], k - 1)
             if any([x not in itemsets for x in subsets]):
-                candidate.remove(c)
-
+                candidate.pop(idx)
         return set(candidate)
 
     def rsaa_gen2(self, itemset1, itemset2, k):
-        candidate = [tuple(sorted(set(x).union([y]))) for x in itemset1 for y in itemset2 if set(x) != set(y)]
+        candidate = [tuple(sorted(set(x).union(y))) for x in itemset1 for y in itemset2 if set(x) != set(y)]
         candidate = [x for x in candidate if len(x) == k]
         return set(candidate)
 
     def gen_quasi(self, candidate):  # NC or NLC
-        ret = set()  # NL or NLL
+        ret = set()
         cands_temp = set()
         for trans in self.data:
             for cand in candidate:
@@ -75,10 +74,12 @@ class RelativeSupportApriori:
                     cands_temp.add(cand)
         for cand in cands_temp:
             if self.freq_dict[cand] >= self.min_sup:
-                r_sup_b = min([self.freq_dict[tuple[item]] for item in cand])
-                r_sup = self.freq_dict[cand] / r_sup_b
+                r_sup_b = min([self.freq_dict[(item,)] for item in cand])
+                count = self.freq_dict[cand]
+                r_sup = count / r_sup_b
                 if r_sup >= self.min_r_sup:
                     ret.add(cand)
+                    self.rule_input[frozenset(cand)] = count
         return ret
 
     def gen_subsets(self, items, m):
@@ -88,6 +89,7 @@ class RelativeSupportApriori:
 
 
 if __name__ == '__main__':
-    data = [(1, 2, 3), (2,), (1, 4), (1, 3, 4), (2, 3), (4, 5)]
-    rsaa = RelativeSupportApriori(data, 1, 3, 0.5, 0)
+    data = [(1, 2, 3), (3, 4), (1, 2, 3, 4, 5, 6, 7), (6, 7), (3, 4, 5, 6), (1, 2), (3, 4, 6, 7), (1, 2),
+            (1, 3, 4, 5, 6), (1, 2)]
+    rsaa = RelativeSupportApriori(data, 2, 4, 0.7, 0)
     print(rsaa.get_candidate_itemsets())
