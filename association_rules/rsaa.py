523b2dd6053932663125d2fcabff0e354fed1039
wxin_bxbank <weixin@aibank.com>
2019-04-25 19:42:20 +0800

    rsaa

--- association_rules/rsaa.py
+++ association_rules/rsaa.py
@@ -4,26 +4,32 @@ import itertools
 
 class RelativeSupportApriori:
 
-    def __init__(self, data, min_sup, max_sup, r_sup, min_conf):
+    def __init__(self, data, min_sup, max_sup, r_sup):
         self.data = data
         self.min_sup = min_sup
         self.max_sup = max_sup
         self.min_r_sup = r_sup
-        self.min_conf = min_conf
 
         self.freq_dict = defaultdict(int)
         self.F1 = set()  # F==L,
         self.R1 = set()  # R==NL
-        self.rule_input = defaultdict(int)
+        self.candi_itemsets = defaultdict(int)  # >= r_sup
+        self.helper_itemsets = defaultdict(int)  # < r_sup
 
     def get_candidate_itemsets(self):
         for trans in self.data:
             for item in trans:
-                self.freq_dict[tuple([item])] += 1
-                self.rule_input[frozenset({item})] += 1
+                self.freq_dict[frozenset({item})] += 1
 
-        self.F1 = set([item for item, support in self.freq_dict.items() if support >= self.max_sup])
-        self.R1 = set([item for item, support in self.freq_dict.items() if self.max_sup > support >= self.min_sup])
+        self.helper_itemsets = defaultdict(int,
+                                           [(frozenset(item), support) for item, support in self.freq_dict.items() if
+                                            support >= self.max_sup])
+        self.candi_itemsets = defaultdict(int,
+                                          [(frozenset(item), support) for item, support in self.freq_dict.items() if
+                                           self.max_sup > support >= self.min_sup])
+
+        self.F1 = set(self.helper_itemsets.keys())
+        self.R1 = set(self.candi_itemsets.keys())
 
         NC2 = self.rsaa_gen(self.R1, 2)  # NC2
         NLC2 = self.rsaa_gen2(self.R1, self.F1, 2)  # NL2
@@ -39,30 +45,33 @@ class RelativeSupportApriori:
             NLL = self.gen_quasi(NLC)
             k += 1
 
-        return self.rule_input
+        return self.candi_itemsets, self.helper_itemsets
 
     def rsaa_gen(self, itemsets, k):
         if k == 2:
-            candidate = [tuple(sorted([x[0], y[0]])) for x in itemsets for y in itemsets if
-                         len((x[0], y[0])) == k and x[0] != y[0]]
+            candidate = [frozenset(sorted(x.union(y))) for x in itemsets for y in itemsets if x != y]
+            # candidate = [tuple(sorted([x[0], y[0]])) for x in itemsets for y in itemsets if
+            #              len((x[0], y[0])) == k and x[0] != y[0]]
         else:
             candidate = list(itertools.combinations(itemsets, 2))
             candidate = [set(i[0]).union(i[1]) for i in candidate]
-            candidate = [tuple(sorted(i)) for i in candidate if len(i) == k]
+            candidate = [frozenset(sorted(i)) for i in candidate if len(i) == k]
 
-        candidate = list(set(candidate))
-        for idx in range(len(candidate)-1, -1, -1):
-            subsets = self.gen_subsets(candidate[idx], k - 1)
-            if any([x not in itemsets for x in subsets]):
-                candidate.pop(idx)
+        # candidate = list(set(candidate))
+        # for idx in range(len(candidate)-1, -1, -1):
+        #     subsets = self.gen_subsets(candidate[idx], k - 1)
+        #     if any([x not in itemsets for x in subsets]):
+        #         candidate.pop(idx)
         return set(candidate)
 
     def rsaa_gen2(self, itemset1, itemset2, k):
-        candidate = [tuple(sorted(set(x).union(y))) for x in itemset1 for y in itemset2 if set(x) != set(y)]
+        candidate = [frozenset(sorted(set(x).union(y))) for x in itemset1 for y in itemset2 if x != y]
         candidate = [x for x in candidate if len(x) == k]
         return set(candidate)
 
     def gen_quasi(self, candidate):  # NC or NLC
+        if len(candidate) == 0:
+            return candidate
         ret = set()
         cands_temp = set()
         for trans in self.data:
@@ -74,12 +83,14 @@ class RelativeSupportApriori:
                     cands_temp.add(cand)
         for cand in cands_temp:
             if self.freq_dict[cand] >= self.min_sup:
-                r_sup_b = min([self.freq_dict[(item,)] for item in cand])
+                r_sup_b = min([self.freq_dict[frozenset({item})] for item in cand])
                 count = self.freq_dict[cand]
                 r_sup = count / r_sup_b
                 if r_sup >= self.min_r_sup:
                     ret.add(cand)
-                    self.rule_input[frozenset(cand)] = count
+                    self.candi_itemsets[cand] = count
+                else:
+                    self.helper_itemsets[cand] = count
         return ret
 
     def gen_subsets(self, items, m):
@@ -89,7 +100,25 @@ class RelativeSupportApriori:
 
 
 if __name__ == '__main__':
-    data = [(1, 2, 3), (3, 4), (1, 2, 3, 4, 5, 6, 7), (6, 7), (3, 4, 5, 6), (1, 2), (3, 4, 6, 7), (1, 2),
-            (1, 3, 4, 5, 6), (1, 2)]
-    rsaa = RelativeSupportApriori(data, 2, 4, 0.7, 0)
-    print(rsaa.get_candidate_itemsets())
+    from pymining import itemmining
+    import orangecontrib.associate.fpgrowth as oaf
+
+    # data = [(1, 2, 3), (3, 4), (1, 2, 3, 4, 5, 6, 7), (6, 7), (3, 4, 5, 6), (1, 2), (3, 4, 6, 7), (1, 2),
+    #         (1, 3, 4, 5, 6), (1, 2)]
+    # ds = data
+    # min_sup = 2
+    # max_sup = 4
+    # r_sup = 0.7
+    # min_conf = 0.6
+    #
+    # relim_input = itemmining.get_relim_input(data)
+    # freq_itemsets = itemmining.relim(relim_input, min_support=max_sup)
+    #
+    # rsa = RelativeSupportApriori(ds, min_sup, max_sup, r_sup)
+    # candi_itemsets, helper_itemsets = rsa.get_candidate_itemsets()
+    #
+    # helper_itemsets = dict(helper_itemsets.items() | freq_itemsets.items())
+    # rules = gen_rules.association_rules(candi_itemsets, helper_itemsets, min_conf)
+
+    # rules_info = list(fpgrowth.rules_stats(rules, dict(helper_itemsets.items() | candi_itemsets.items()), 10))  # 统计评估信息
+    # print(rules_info)
